9473ed38294a52487f620a27d432d6a81b448673
287000 <dannyphiri287@gmail.com>
2026-09-22 15:05:13 +0200

    Update ncss.js

--- Clinical Neural Link/Frontend/Frontend/js/ncss.js
+++ Clinical Neural Link/Frontend/Frontend/js/ncss.js
@@ -1293,6 +1293,7 @@ window.CAMPUS_IMAGES = [
     '../assets/icons/cbu_som_2.jpg',
     '../assets/icons/cbu_som_3.jpg',
     '../assets/icons/cbu_som_4.jpg'
+    '../assets/icons/cbu_som_5.jpg'
 ];
 
 window.loginSlideshowTimer = null;
